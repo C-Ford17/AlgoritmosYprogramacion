9f245f11abe211d26d4fe78cdaab1c5c2893a03a
C-Ford17 <83851979+C-Ford17@users.noreply.github.com>
2021-12-07 19:31:00 -0500

    Add files via upload

--- champion.py
+++ champion.py
@@ -1,84 +1,104 @@
-import datetime
 import json
 from equipo import Equipo
 from jugador import Jugador
+from fecha import Fecha
+import random
 class Champion:
-    equipos= {}
-    equipos["equipos"]=[]
-    with open('data.json') as file:
-        equipos = json.load(file)
+    equipos ={}
+    FechaNumero=[]
 
     def __init__(self):
-        pass
-
-    def __repr__(self):
-        for equipos in self.equipos:
-            return f"{self.equipos[equipos.nombre]} : {self.equipos[equipos.nombre.pais]}"
-
+        if self.equipos:
+            with open('data.json') as file:
+                self.equipos=json.load(file)            
+        else:
+            self.equipos = {"###": {
+            "nombre": "###",
+            "pais": "###",
+            "apodo": "###",
+            "estadio": "###",
+            "fechaDeFundacion": {
+                "dia": 1,
+                "mes": 1,
+                "a\u00f1o": 1
+            },
+            "jugadores": {},
+            "continente": None,
+            "puntos":0
+            }}
+        
+                
     def generarcalendario(self):
+        for i in range(38):
+            dia=random.randint(1,28)
+            mes=random.randint(1,12)
+            año=2022
+            fecha = (dia,mes,año)
+            hora = (random.randint(1,24),random.randint(1,60))
+            self.FechaNumero.append({f"fecha{i+1}":fecha,"hora":hora})
+            with open('calendario.json','w') as file:
+                json.dump(self.FechaNumero, file, indent=4)
+
+    def guardarenjson(self):
+        with open('data.json','w') as file:
+            json.dump(self.equipos, file, indent=4)
+
+    def partido(self):
         pass
 
     def agregarequipo(self):
         equipoNuevo = Equipo.crearequipo()
-        self.equipos["equipos"].append({
+        self.equipos.update({equipoNuevo.nombre:{}})
+        self.equipos[equipoNuevo.nombre].update({
             "nombre":equipoNuevo.nombre,
             "pais":equipoNuevo.pais,
             "apodo":equipoNuevo.apodo,
             "estadio":equipoNuevo.estadio,
             "fechaDeFundacion":equipoNuevo.fechadefundacion,
-            "jugadores":equipoNuevo.jugadores["jugadores"],
-            "continente":None
-        })
-        with open('data.json','w') as file:
-            json.dump(self.equipos.copy(), file, indent=4)
+            "jugadores":equipoNuevo.jugadores,
+            "continente":None,
+            "puntos":equipoNuevo.puntos
+            })
+        self.guardarenjson()
 
     def verequipos(self, s):
-        for equipos in self.equipos["equipos"]:
-            if s == "n":
-                print({equipos["nombre"]:equipos["continente"]})
+        for equipos in self.equipos:
+            if s =="s":
+                print(equipos)
             else:
-                print(equipos["nombre"])
+                print(equipos,",",self.equipos[equipos]["continente"])
 
     def eliminarequipo(self, equipo):
-        for equipos in self.equipos["equipos"]:
-            if equipos["nombre"] ==equipo:
-                del(equipos)
-                with open('data.json','w') as file:
-                    json.dump(self.equipos.copy(), file, indent=4)
+        for equipos in self.equipos:
+            if equipos ==equipo:
+                del self.equipos[equipo]
+                print("se ha eliminado el equipo")
+                self.guardarenjson()
 
     def equipoContinente(self):
-        for equipos in self.equipos["equipos"]:
-            if equipos["pais"] in ["Albania","Alemania","Andorra","Armenia","Austria","Azerbaiyán",
-            "Bélgica","Bielorrusia","Bosnia y Herzegovina","Bulgaria","Chipre","Croacia","Dinamarca",
-            "Eslovaquia","Eslovenia","España","Estonia","Finlandia","Francia","Georgia","Grecia","Hungría",
-            "Inglaterra","Irlanda","Islandia","Italia","Letonia","Liechtenstein","Lituania","Luxemburgo",
-            "Macedonia del Norte","Malta","Moldavia","Mónaco","Montenegro","Noruega","Países Bajos","Polonia",
-            "Portugal","Reino Unido","República Checa","Rumania","Rusia","San Marino","Serbia","Suecia",
-            "Suiza","Ucrania","Unión Europea","Vaticano"]:
-                equipos["continente"]="Europa"
-                with open('data.json','w') as file:
-                    json.dump(self.equipos.copy(), file, indent=4)
-
-            if equipos["pais"] in ["Brasil","Argentina","Colombia","Perú","Chile","Ecuador","Ecuador",
-            "Venezuela","Bolivia","Uruguay","Guyana","Surinam","Paraguay","Guyana Francesa","Aruba",
-            "Islas Malvinas","Curazao","Trinidad y Tobago","Caribe Neerlandés"]:
-                equipos["continente"]="Suramerica"
-                with open('data.json','w') as file:
-                    json.dump(self.equipos.copy(), file, indent=4)
+        for equipos in self.equipos:
+            if self.equipos[equipos]["pais"] in ["Albania","Alemania","Andorra","Armenia","Austria","Azerbaiyán","Bélgica","Bielorrusia","Bosnia y Herzegovina","Bulgaria","Chipre","Croacia","Dinamarca","Eslovaquia","Eslovenia","España","Estonia","Finlandia","Francia","Georgia","Grecia","Hungría","Inglaterra","Irlanda","Islandia","Italia","Letonia","Liechtenstein","Lituania","Luxemburgo","Macedonia del Norte","Malta","Moldavia","Mónaco","Montenegro","Noruega","Países Bajos","Polonia","Portugal","Reino Unido","República Checa","Rumania","Rusia","San Marino","Serbia","Suecia","Suiza","Ucrania","Vaticano"]:
+                self.equipos[equipos]["continente"]="Europa"
+                self.guardarenjson()
+            if self.equipos[equipos]["pais"] in ["Brasil","Argentina","Colombia","Perú","Chile","Ecuador","Ecuador",
+                "Venezuela","Bolivia","Uruguay","Guyana","Surinam","Paraguay","Guyana Francesa","Aruba",
+                "Islas Malvinas","Curazao","Trinidad y Tobago","Caribe Neerlandés"]:
+                self.equipos[equipos]["continente"]="Suramerica"
+                self.guardarenjson()
 
     def agregarjugadoraequipo(self, equipo):
-        for equipos in self.equipos["equipos"]:
-            if equipos["nombre"]==equipo:
+        for equipos in self.equipos:
+            if equipos==equipo:
                 jugadornuevo = Jugador.ingresarjugador()
-                equipos["jugadores"].append({
+                self.equipos[equipos]["jugadores"].update({
                     "nombre":jugadornuevo.nombre,
                     "pais":jugadornuevo.pais,
                     "posicion":jugadornuevo.posicion,
                     "dorsal":jugadornuevo.dorsal,
-                    "fechaDeNacimiento":jugadornuevo.fdn
+                    "fechaDeNacimiento":jugadornuevo.fdn,
+                    "puntos":jugadornuevo.puntos
                     })
-                with open('data.json','w') as file:
-                    json.dump(self.equipos.copy(), file, indent=4)
+                self.guardarenjson()
             else:
                 "Equipo no encontrado"
 
